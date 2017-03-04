845999f88f0ab36fe0bd77c18663cc7980daece1
Dacharat Pankong <dacharat.p@ku.th>
2017-03-04 22:50:00 +0700

    Update FileUtil.java

--- src/ku/util/FileUtil.java
+++ src/ku/util/FileUtil.java
@@ -11,18 +11,26 @@ import java.io.PrintWriter;
 
 import javax.management.RuntimeErrorException;
 
+/**
+ * Copy InputStream to the OutputStream.
+ * 
+ * @author Dacharat Pankong
+ *
+ */
 public class FileUtil {
-	private InputStream in = null;
 	
-	private OutputStream out = null;
-	
+	/**
+	 * Copy InputStream to OutputStream one byte at a time
+	 * 
+	 * @param in is the InputStream that file will read.
+	 * @param out is the OutputStream that file will write.
+	 * @throws RuntimeException if it cannot copy file to OutputStream.
+	 */
 	static void copy(InputStream in,OutputStream out) {
 		try{
-			//StringBuffer buffer = new StringBuffer();
 			while(true){
 				int c = in.read();
 				if(c < 0 ) break;
-				//buffer.append((char)c);
 				out.write(c);
 			}
 			in.close();
@@ -34,6 +42,14 @@ public class FileUtil {
 		
 	}
 	
+	/**
+	 * Copy InputStream to OutputStream by byte array of blocksize.
+	 * 
+	 * @param in is the InputStream that file will read.
+	 * @param out is the OutputStream that file will write.
+	 * @param blocksize is how many byte to copy at time.
+	 * @throws RuntimeException if it cannot copy file to OutputStream.
+	 */
 	static void copy(InputStream in, OutputStream out, int blocksize) {
 		try{
 			byte[] buffer = new byte[blocksize];
@@ -51,6 +67,13 @@ public class FileUtil {
 		
 	}
 	
+	/**
+	 * Copy InputStream to OutputStream by using BufferedReader and PrintWriter.
+	 * 
+	 * @param in is the InputStream that file will read.
+	 * @param out is the OutputStream that file will write.
+	 * @throws RuntimeException if it cannot copy file to OutputStream.
+	 */
 	static void bcopy(InputStream in, OutputStream out){
 		BufferedReader breader = new BufferedReader( new InputStreamReader(in));
 		PrintWriter print = new PrintWriter(out);
@@ -68,6 +91,14 @@ public class FileUtil {
 		}		
 	}
 	
+	/**
+	 * Copy InputStream to OutputStream by using BufferedWriter and BufferedReader.
+	 * 
+	 * @param in is the InputStream that file will read.
+	 * @param out is the OutputStream that file will write.
+	 * @param blocksize is how many byte to copy at time.
+	 * @throws RuntimeException if it cannot copy file to OutputStream.
+	 */
 	static void copyChar(InputStream in, OutputStream out, int blocksize){
 		BufferedReader breader = new BufferedReader(new InputStreamReader(in));
 		BufferedWriter print = new BufferedWriter(new OutputStreamWriter(out));
